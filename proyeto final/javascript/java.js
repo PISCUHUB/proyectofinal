9c9a7fabe73e1ad918b380ac67ab71fb577f7de0
PISCUHUB <seba_bernal@hotmail.com>
2022-10-03 18:07:35 -0300

    proyectofinal

--- proyeto final/javascript/java.js
+++ proyeto final/javascript/java.js
@@ -1,13 +1,16 @@
 /* ABONDANA LA PAGINA*/
 window.onbeforeunload = preguntarAntesDeSalir;
-function preguntarAntesDeSalir()
-{
+function preguntarAntesDeSalir() {
     return "¿Seguro que quieres salir?";
 }
 /* PREGUNTA DE EDAD*/
-let edad = Number(prompt("Introduce tu edad."));
-if (edad >= 18) {
-    alert("puedes entrar, eres mayor de edad.");
-} else {
-    alert("no eres mayor de edad, estas bajo su responsabilidad.");
-};
+while (true) {
+    var edad = Number(prompt("Introduce tu edad."));
+    if (edad >= 18) {
+        alert("puedes entrar, eres mayor de edad.");
+        break;
+    } else {
+        alert("¡no eres mayor de edad ! , no puedes ingresar...");
+    } 
+    
+}
